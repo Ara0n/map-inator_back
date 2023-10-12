e0cb1ca83a1697bd8281bbc4add4cc52bf4af718
Ara0n <thomas99.montero@gmail.com>
2023-10-13 00:54:01 +0200

    added neo4j connection

---
+++ src/neo4j.ts
@@ -0,0 +1,13 @@
+import { auth, driver } from "neo4j-driver"
+
+if (!process.env.NEO4J_URI) throw new Error("neo4j: please add a URI")
+if (!process.env.NEO4J_LOGIN) throw new Error("neo4j: please add a login")
+if (!process.env.NEO4J_PASSWORD) throw new Error("neo4j: please add a password")
+
+export const neo4j_driver = driver(
+	process.env.NEO4J_URI,
+	auth.basic(process.env.NEO4J_LOGIN, process.env.NEO4J_PASSWORD),
+)
+
+// console.log("neo4j")
+// console.log(await neo4j_driver.getServerInfo())
