c165948cc382c703730ae3cadcf7168c4f86bd71
Ara0n <thomas99.montero@gmail.com>
2023-10-13 03:17:39 +0200

    add city endpoint

--- src/routes/city.ts
+++ src/routes/city.ts
@@ -1,5 +1,5 @@
 import KoaRouter from "@koa/router"
-// import { auth, driver } from "neo4j-driver"
+import { neo4j_driver } from "../neo4j.ts"
 
 const router = new KoaRouter({ prefix: "/api/city" })
 export default router
@@ -18,8 +18,16 @@ router.post("/:name", () => {
 	// add the city info in neo4j
 })
 
-router.put("/:name", ctx => {
+router.put("/:name", async ctx => {
 	// modify an existing city
 	console.log(ctx.request.body)
+	console.log(await neo4j_driver.getServerInfo())
+
+	const res = await neo4j_driver.executeQuery(
+		"MERGE (c:City {name: $body.cityName, lat: $body.lat, lng: $body.lng, hasTree: $body.hasTree, hasCircle: $body.hasCircle})",
+		{ body: ctx.request.body },
+	)
+
+	console.log(res)
 	ctx.status = 204
 })
